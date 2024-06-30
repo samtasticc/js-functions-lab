a0f956955fbc595667cea824546a31d4a03bec49
samtasticc <samantha.henesy@gmail.com>
2024-06-30 12:00:24 -0400

    excercise 1 and 2

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+function isAdult() {
+    if (adult = 18)
+        console.log('Adult') 
+    } else {
+        console.log('Minor')       
+}
+
+console.log('Exercise 2 Result:', isAdult(21)); // i changed my code so many times and i received various syntax errors. when my code did work, i received a bool (true) instead of the intended output.
+
+
