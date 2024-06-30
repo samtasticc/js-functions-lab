8257b087d53cb6b7c171ba992790c9eeab2165ab
samtasticc <samantha.henesy@gmail.com>
2024-06-30 12:43:19 -0400

    exercise 3

--- script.js
+++ script.js
@@ -33,10 +33,34 @@ Complete the exercise in the space below:
 function isAdult() {
     if (adult = 18)
         console.log('Adult') 
-    } else {
-        console.log('Minor')       
+    // } else {
+    //     console.log('Minor')       
 }
 
-console.log('Exercise 2 Result:', isAdult(21)); // i changed my code so many times and i received various syntax errors. when my code did work, i received a bool (true) instead of the intended output.
+console.log('Exercise 2 Result:', isAdult(21)); // i changed my code so many times and i received various syntax errors. when my code did work, i received a bool (true) instead of the intended output. my current code keeps telling me "else" is unexpected.
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+function isCharAVowel(vowel) {
+    if (vowel === "a", "e", "i", "o", "u")
+        console.log(true) /*{
+    } else {
+        console.log(false)
+    }*/
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a")); // i keep receiving the error that "else" was unexpected.
+
 
 
