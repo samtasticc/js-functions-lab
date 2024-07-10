b7442e2848912a6eed12f88fbab25571caafae3c
samtasticc <samantha.henesy@gmail.com>
2024-07-09 22:35:38 -0400

    updated excercises 2-6

--- script.js
+++ script.js
@@ -30,16 +30,15 @@ Example: isAdult(21) should return 'Adult'.
 Complete the exercise in the space below:
 */
 
-// function isAdult() {
-//     if (adult = 18)
-//         console.log('Adult') 
-//     // } else {
-//     //     console.log('Minor')       
-// }
+function isAdult(adult) {
+    if (adult >= 18) {
+        return ('Adult')
+    } else {
+        return ('Minor')
+    }
+}
 
-// console.log('Exercise 2 Result:', isAdult(21)); 
-// // i changed my code so many times and i received various syntax errors. when my code did work, i received a bool (true)
-// // instead of the intended output. my current code keeps telling me "else" is unexpected.
+console.log('Exercise 2 Result:', isAdult(35));
 
 /*
 Exercise 3: isCharAVowel()
@@ -54,40 +53,39 @@ Example: isCharAVowel('a') should return true.
 Complete the exercise in the space below:
 */
 
-// function isCharAVowel(vowel) {
-//     if (vowel === "a", "e", "i", "o", "u")
-//         console.log(true) /*{
-//     } else {
-//         console.log(false)
-//     }*/
-// }
+function isCharAVowel(vowel) {
+    if (vowel === "a", "e", "i", "o", "u") {
+        return (true)
+    } else {
+        return (false)
+    }
+}
 
-// console.log('Exercise 3 Result:', isCharAVowel("a")); // i keep receiving the error that "else" was unexpected.
+console.log('Exercise 3 Result:', isCharAVowel("a")); 
 
 /*
 Exercise 4: generateEmail()
 
-Create a function named generateEmail. It should take two strings: 
+Create a function named generateEmail. It should take two strings:
 a name and a domain. It should return a simple email address.
 
-Example: generateEmail('johnsmith', 'example.com') 
+Example: generateEmail('johnsmith', 'example.com')
 should return 'johnsmith@example.com'.
 
 Complete the exercise in the space below:
 */
 
-// function generateEmail(name, dotCom) {
-//     console.log('samantha.henesy', '@gmail.com')
-//     // it returns "samantha.henesy @gmail.com", how do I remove the space?
-// }
+function generateEmail(name, dotCom) {
+    return (name + '@' + dotCom)
+}
 
-// console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+console.log('Exercise 4 Result:', generateEmail("samantha.henesy", "gmail.com"));
 
 /*
 Exercise 5: greetUser()
 
-Define a function called greetUser. It should take a name and a 
-time of day (morning, afternoon, evening) and return a personalized 
+Define a function called greetUser. It should take a name and a
+time of day (morning, afternoon, evening) and return a personalized
 greeting.
 
 Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
@@ -95,18 +93,18 @@ Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
 Complete the exercise in the space below:
 */
 
-// function greetUser(timeOfDay, name) {
-//     console.log(`Good ${timeOfDay}, ${name}`);
-// }
-// greetUser('morning', 'Sam')
+function greetUser(timeOfDay, name) {
+    return (`Good ${name}, ${timeOfDay}`);
+}
+greetUser('morning', 'Sam')
 
-// console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
-// i got the correct response (good morning, Sam), then the backwords response (good Sam, morning), and undefined in the console. 
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
 
 /*
 Exercise 6: maxOfThree()
 
-Define a function, maxOfThree. It should accept three numbers 
+Define a function, maxOfThree. It should accept three numbers
 and return the largest among them.
 
 Example: maxOfThree(17, 4, 9) should return 17.
@@ -114,19 +112,23 @@ Example: maxOfThree(17, 4, 9) should return 17.
 Complete the exercise in the space below:
 */
 
-// function maxOfThree (num1, num2, num3) {
+function maxOfThree (num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1
+    } else if (num2 > num1 && num2 > num3) {
+        return num2
+    } else {
+        return num3
+    }
+}
 
-// }
-// // i couldn't figure this one out. googling it resulted in a lot of stuff about arrays, which wasn't covered yet and
-// // i did not want to move ahead. the only other way i could find was using Math.max() which we also didn't cover.
-
-// console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
 /*
 Exercise 7: calculateTip()
 
-Create a function called calculateTip. It should take two arguments: 
-the bill amount and the tip percentage (as a whole number). 
+Create a function called calculateTip. It should take two arguments:
+the bill amount and the tip percentage (as a whole number).
 The function should return the amount of the tip.
 
 Example: calculateTip(50, 20) should return 10.
@@ -134,19 +136,19 @@ Example: calculateTip(50, 20) should return 10.
 Complete the exercise in the space below:
 */
 
-// function calculateTip (billTotal){
-//     const tip = billTotal * (30 / 100);
-//     return tip;
-// }
+function calculateTip (billTotal){
+    const tip = billTotal * (30 / 100);
+    return tip;
+}
 
-// console.log('Exercise 7 Result:', calculateTip(50, 30));
+console.log('Exercise 7 Result:', calculateTip(50, 30));
 
 /*
 Exercise 8: convertTemperature()
 
-Write a function named convertTemperature. 
-It takes two arguments: a temperature and a string representing the 
-scale ('C' for Celsius, 'F' for Fahrenheit). 
+Write a function named convertTemperature.
+It takes two arguments: a temperature and a string representing the
+scale ('C' for Celsius, 'F' for Fahrenheit).
 Convert the temperature to the other scale.
 
 Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
@@ -155,22 +157,22 @@ Example: convertTemperature(32, 'F') should return 0 (Celsius).
 Complete the exercise in the space below:
 */
 
-// function convertTemperature (newTemp) {
-//     const cToF = (newTemp * 9/5) + 32
-//     return cToF;
-// }
+function convertTemperature (newTemp) {
+    const cToF = (newTemp * 9/5) + 32
+    return cToF;
+}
 
-// console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
 
 /*
 Exercise 9: basicCalculator()
 
-Create a function named basicCalculator. 
-It should take three arguments: two numbers and a string representing 
-an operation ('add', 'subtract', 'multiply', 'divide'). 
-Perform the provided operation on the two numbers. 
-In operations where the order of numbers is important, 
-treat the first parameter as the first operand and the 
+Create a function named basicCalculator.
+It should take three arguments: two numbers and a string representing
+an operation ('add', 'subtract', 'multiply', 'divide').
+Perform the provided operation on the two numbers.
+In operations where the order of numbers is important,
+treat the first parameter as the first operand and the
 second parameter as the second operand.
 
 Example: basicCalculator(10, 5, 'subtract') should return 5.
